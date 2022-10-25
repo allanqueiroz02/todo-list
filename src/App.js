b67d57d909b25c91569a75c0dad857c78a30b149
Allan Q <allan.queiroz.dev@gmail.com>
2022-10-24 22:31:00 -0300

    adicionado appBar, com dark mode icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { Fragment } from "react";
+
 import "./global.css";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+
 import Home from "./Pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Fragment>
+      <AppBar position="relative">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h3">Cool To do LIST</Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <DarkModeIcon />
+          </IconButton>
+        </Box>  
+      </AppBar>
+      <Home />
+    </Fragment>
+  );
 }
 
 export default App;
