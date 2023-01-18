33d0c8b912ed865f6a20bcacc547bc5504cf5e23
Allan <allan.queiroz.dev@gmail.com>
2023-01-18 14:12:04 -0300

    alterado estilizacao

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,6 +1,16 @@
 import styled from "styled-components";
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  margin: 13px;
+  opacity: 0.7;
+
+  :hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+`;
 
 function CustomButton({ children, onClick, disabled = false, title }) {
   return (
