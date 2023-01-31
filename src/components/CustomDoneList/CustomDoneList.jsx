5a07df72551d9195b09d431bbd9e1ec7776d79ad
Allan <allan.queiroz.dev@gmail.com>
2023-01-30 21:05:51 -0300

    novo componente

---
+++ src/components/CustomDoneList/CustomDoneList.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import undoIcon from "../../assets/undo.png";
+
+const StyledList = styled.ul`
+  list-style-type: none;
+  padding: 20px;
+`;
+
+const StyledListItem = styled.li`
+  padding: 10px;
+  margin: 5px;
+  border-radius: 3px;
+  border: none;
+  border-bottom: 3px solid #f1f1f1;
+  border-left: 3px solid #f1f1f1;
+  :hover {
+    background-color: #cfcfcf;
+  }
+`;
+
+const StyledImg = styled.img`
+  width: 25px;
+  height: 25px;
+  padding: 3px;
+  margin: 3px;
+  background-color: #e9e6e6;
+  border-radius: 50%;
+  opacity: 0.6;
+  :hover {
+    opacity: 1;
+    cursor: pointer;
+  }
+`;
+
+function CustomDoneList({ doneList, setDoneList }) {
+  return (
+    <StyledList>
+      {doneList.map((itemList, indexItem) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+          }}
+          key={`${itemList}${Math.random() * 50}`}
+        >
+          <StyledListItem style={{ width: "90%" }}>{itemList}</StyledListItem>
+          <StyledImg
+            src={undoIcon}
+            alt="undo icon"
+            title={`Mover ${doneList[indexItem]} para cima`}
+            onClick={() => console.log("Subiu o item")}
+          />
+        </div>
+      ))}
+    </StyledList>
+  );
+}
+
+export default CustomDoneList;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import CustomButton from "./CustomButton/CustomButton";
+import CustomDoneList from "./CustomDoneList/CustomDoneList";
 import CustomInput from "./CustonInput/CustomInput";
 import CustomList from "./CustomList/CustomList";
 import CustomTitle from "./CustomTitle/CustomTitle";
 
-export { CustomButton, CustomInput, CustomList, CustomTitle };
+export { CustomButton, CustomDoneList, CustomInput, CustomList, CustomTitle };
